264259b07858eef9a75d1edd1f9045ca9f63b720
James Winkle <jwin4740@gmail.com>
2017-09-05 00:41:28 -0400

    10 second skip working

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,14 @@
 var hamilton = new Howl({
-    src: ['./audio/firstSix.mp3'],
+    src: ['./audio/throughSatisfied.mp3'],
     rate: 1
-
 });
-
+var hamiltonArray = [80.37866666666667,
+    84.17066666666666, 167.66933333333333,
+    169.73866666666666,
+    181.90933333333334,
+    183.70933333333332, 232.34933333333333];
 var hamLength;
+var timestamp;
 
 $('#play').on('click', function () {
     hamilton.play();
@@ -19,9 +23,15 @@ $('#stop').on('click', function () {
 });
 
 $('#currentTime').on('click', function () {
-    var timestamp = hamilton.seek();
-    $('#timestamps').append(timestamp + '<br>');
+    currentTime();
 });
+
+function currentTime() {
+    var timestamp = hamilton.seek();
+    console.log(timestamp);
+    $('#timestamps').append(timestamp + '<br>');
+    return timestamp;
+}
 
 $('#custTime').on('click', function () {
     var req = parseFloat($('#customTime').val());
@@ -41,7 +51,13 @@ $('#jump').on('click', function () {
     hamilton.play();
 });
 
-
+$('#jumpTen').on('click', function () {
+   var now = hamilton.seek();
+   console.log(typeof(now));
+    hamilton.stop();
+    hamilton.seek(now + 10);
+    hamilton.play();
+});
 
 
 hamilton.on("load", function () {
