ae345c40f2c28c96d7bce1708cc81988035f9fe1
James Winkle <jwin4740@gmail.com>
2017-09-04 20:55:12 -0400

    host on local server

---
+++ public/javascripts/main.js
@@ -0,0 +1,46 @@
+var hamilton = new Howl({
+        src: ['./audio/alexanderHamilton.mp3'],
+        rate: 1
+
+    });
+
+    $('#play').on('click', function () {
+        hamilton.play();
+    });
+
+    $('#pause').on('click', function () {
+        hamilton.pause();
+    });
+
+    $('#stop').on('click', function () {
+        hamilton.stop();
+    });
+
+    $('#currentTime').on('click', function () {
+        var timestamp = hamilton.seek();
+        $('#timestamps').append(timestamp + '<br>');
+    });
+
+    $('#custTime').on('click', function () {
+        var req = parseFloat($('#customTime').val());
+        console.log(req);
+        hamilton.stop();
+        hamilton.seek(req);
+        hamilton.play();
+        $('#customTime').val("");
+
+    });
+
+    // 167.608 jump to hamilton
+
+    $('#jump').on('click', function () {
+        hamilton.stop();
+        hamilton.seek(goRand());
+        hamilton.play();
+    });
+
+
+    function goRand(){
+        tem = Math.floor(Math.random()*230 + 10);
+        return tem;
+    }
