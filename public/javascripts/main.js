69d55d2dfb79a40011f090292b3b78647dcdd64d
James Winkle <jwin4740@gmail.com>
2017-09-05 01:12:57 -0400

    tweak hamilton timing

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,8 +5,10 @@ var hamilton = new Howl({
 var hamiltonArray = [80.37866666666667,
     84.17066666666666, 167.66933333333333,
     169.73866666666666,
-    181.90933333333334,
-    183.70933333333332, 232.34933333333333];
+    181.75,
+    183.70933333333332, 232.34933333333333, 252.96, 1437.1493333333333, 1502.8613333333333, 1523.9493333333335,
+    1635.7386666666666, 1638.2293333333334, 1641.9786666666669, 2047.4986666666666];
+var hamArrayLen = hamiltonArray.length;
 var hamLength;
 var timestamp;
 
@@ -47,7 +49,7 @@ $('#custTime').on('click', function () {
 
 $('#jump').on('click', function () {
     hamilton.stop();
-    hamilton.seek(goRand());
+    hamilton.seek(goRandHamilton());
     hamilton.play();
 });
 
@@ -69,4 +71,10 @@ function goRand() {
     tem = (Math.random() * hamLength);
     console.log(tem);
     return tem;
+}
+
+function goRandHamilton() {
+    var ha = Math.floor(Math.random() * hamArrayLen);
+    console.log(hamiltonArray[ha]);
+    return hamiltonArray[ha];
 }
