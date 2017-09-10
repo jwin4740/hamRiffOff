40ac774104a920be6cac84b5460720ba745cac19
James Winkle <jwin4740@gmail.com>
2017-09-10 09:21:00 -0400

    waveform working

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -2,17 +2,30 @@ var hamilton = new Howl({
     src: ['static/audio/throughSatisfied.mp3'],
     rate: 1
 });
+var activeSrc = "static/images/activewave.gif";
+var stillSrc = "static/images/stillwave.gif";
+var waveformGif = $("<img>");
+var waveClass = $(".waveformGif");
+var playQuery = $("#play");
+var pauseQuery = $("#pause");
+$("#playBox").prepend(waveformGif);
+
+
 var hamiltonArray = [1436.95, 252.80, 232.349, 1502.86133, 2047.40, 181.72, 84.12, 1523.7, 80.2, 1635.62];
 var hamArrayLen = hamiltonArray.length;
 var hamLength;
 var timestamp;
 
-$('#play').on('click', function () {
+playQuery.on('click', function () {
     hamilton.play();
+    waveClass.attr("src", activeSrc);
+    waveClass.attr("data-state", "animate");
 });
 
-$('#pause').on('click', function () {
+pauseQuery.on('click', function () {
     hamilton.pause();
+    waveClass.attr("src", stillSrc);
+    waveClass.attr("data-state", "still");
 });
 
 $('#stop').on('click', function () {
@@ -51,8 +64,8 @@ $('#jump').on('click', function () {
 });
 
 $('#jumpTen').on('click', function () {
-   var now = hamilton.seek();
-   console.log(typeof(now));
+    var now = hamilton.seek();
+    console.log(typeof(now));
     hamilton.stop();
     hamilton.seek(now + 10);
     hamilton.play();
